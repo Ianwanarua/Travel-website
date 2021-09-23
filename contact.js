919db5484d4a1630d9c14ef54c170a5eb30d9500
ianwanarua <ian.wanarua@student.moringaschool.com>
2021-09-23 21:32:07 +0300

    Added function to the button

--- contact.js
+++ contact.js
@@ -1,5 +1,10 @@
-function showName(){
-    var name= document.getElementById("send").value;
-
-        alert ( name + "  We have received your message. Thank you for reaching out to us.");
+document.getElementById('sub_btn').addEventListener('click', function () {
+    var userName = $("#fnames").val();
+    var email = $("#email").val();
+    var message = $("#comment").val();
+    if ($("#fnames").val() && $("#email").val() && $("#comment").val()) {
+        alert("Hi" + userName + " we have received your message. Thank you for reaching out to MAGICAL TRIP.");
+    } else {
+        alert("Please fill all fields!");
     }
+  });
