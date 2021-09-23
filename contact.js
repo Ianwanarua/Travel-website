dfa2a0cea9b9314768c8869cb3845641a0889801
ianwanarua <ian.wanarua@student.moringaschool.com>
2021-09-23 21:36:03 +0300

    corrected a spelling error

--- contact.js
+++ contact.js
@@ -1,8 +1,8 @@
 document.getElementById('sub_btn').addEventListener('click', function () {
-    var userName = $("#fnames").val();
+    var userName = $("#fname").val();
     var email = $("#email").val();
     var message = $("#comment").val();
-    if ($("#fnames").val() && $("#email").val() && $("#comment").val()) {
+    if ($("#fname").val() && $("#email").val() && $("#comment").val()) {
         alert("Hi" + userName + " we have received your message. Thank you for reaching out to MAGICAL TRIP.");
     } else {
         alert("Please fill all fields!");
